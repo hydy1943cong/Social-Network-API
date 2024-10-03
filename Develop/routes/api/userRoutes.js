9a1d1124bb7fcbafb215b08f17527fd74517ce07
Cong Wang <77494072+hydy1943cong@users.noreply.github.com>
2024-10-03 17:09:39 -0400

    routes added

---
+++ Develop/routes/api/userRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+  } = require('../../controllers/userController');
+  
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
